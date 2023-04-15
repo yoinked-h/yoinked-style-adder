10dbb34730b282857b4df87c4ea56e74579ffcfb
yoinked <pepe.dannyboy@gmail.com>
2023-04-14 23:10:41 -0700

    v2

--- scripts/styleaddtab.py
+++ scripts/styleaddtab.py
@@ -1,35 +1,69 @@
 import modules.scripts as scripts
 import gradio as gr
-import os, pathlib, csv
-
-from modules import script_callbacks
-
-
+import csv
+from pathlib import Path
+from modules.paths import script_path
+from modules import script_callbacks, paths
+def refresh():
+    csvpath = Path(script_path)
+    with open(csvpath.joinpath("styles.csv"), "r", encoding="utf-8") as f:
+        data = f.read()
+    goods = []
+    rows = csv.reader(data.splitlines())
+    for row in rows:
+        if len(row) != 3:
+            pass #do nothing
+        else:
+            goods.append(row)
+    styles = []
+    for n in goods:
+        if "name" in n[0]:
+            continue
+        styles.append(n[0])
+    return styles
+styles = refresh()
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as ui_component:
-        
-        with gr.Row():
-            styleInput = gr.Textbox(
-                label="Style CSV", lines=5, placeholder='styleName,"positive","negative"'
+        with gr.Tab("Add/Import"):
+            with gr.Row():
+                astyleInput = gr.Textbox(
+                    label="Style CSV", lines=5, placeholder='styleName,"positive","negative"'
+                )
+            with gr.Row():
+                aoutput = gr.components.Text(label="Output")
+                abutton: gr.Button = gr.Button("Add Style")
+            abutton.click(
+                fn=addStyle,
+                inputs=[astyleInput],
+                outputs=[aoutput]
             )
-        with gr.Row():
-            output = gr.components.Text(label="Output")
-            button: gr.Button = gr.Button(label="Add Style")
-        button.click(
-            fn=addStyle,
-            inputs=[styleInput],
-            outputs=[output]
-        )
-        return [(ui_component, "yoinked style adder", "yoinked_style_tab")]
+        with gr.Tab("Export/Delete"):
+            with gr.Row():
+                edropdown = gr.Dropdown(label="Style", choices=styles)
+                erefresh = gr.Button("Refresh")
+                eoutput = gr.components.Text(label="Output")
+                eaction = gr.Dropdown(label="Action", choices=["export", "delete"])
+            erefresh.click(
+                fn=lambda: edropdown.update(choices=refresh()), #i punched a wall to make this code work
+                outputs=[edropdown]
+            )
+
+            with gr.Row():
+                edata = gr.components.Text(label="Data", lines=7)
+            with gr.Row():
+                ebutton: gr.Button = gr.Button("Do action")
+            ebutton.click(
+                fn=actionIze,
+                inputs=[edropdown, eaction],
+                outputs=[edata, eoutput]
+            )
+        return [(ui_component, "yoinked style manager", "yoinked_style_tab")]
 def addStyle(styletxt):
     #verify if the csv is valid
     valid, styletxt = VerifyCSV(styletxt)
     if not valid:
         return styletxt
-    path = pathlib.Path(__file__).parent.absolute()
-    path = path.parent.absolute()
-    path = path.parent.absolute()  #hello os.path users
-    path = path.parent.absolute() 
+    path = Path(script_path) #so from /webui/extensions/yoinked-style-adder/scripts/ to /webui/
     #now that we have the path, we can open the file
     with open(path.joinpath("styles.csv"), "a", encoding="utf-8") as f:
         for obj in styletxt.split("\n"):
@@ -45,8 +79,42 @@ def addStyle(styletxt):
     if len(styletxt.split("\n")) == 1:
         return "Style added successfully!"
     return "Styles added successfully!" #small detail
+def actionIze(style, action):
+    #open the csv
+    path = Path(script_path)
+    with open(path.joinpath("styles.csv"), "r", encoding="utf-8") as f:
+        data = f.read()
+    #read
+    rows = csv.reader(data.splitlines())
+    roz = []
+    for row in rows:
 
-
+        if len(row) != 3:
+            ...
+        else:
+            if row[0] == style:
+                if action == "export":
+                    return  f'{row[0]},"{row[1]}","{row[2]}"', "Copy the data below and send it to share!"
+                elif action == "delete":
+                    pass
+        roz.append(row)
+    if action == "export":
+        return "", "Couldnt find the style in the csv, try pressing the refresh button"
+    elif action == "delete":
+        #for each line in data, if it starts with the style, delete it
+        validation = False
+        for row in data.splitlines():
+            if row.startswith(f"{style},"):
+                data = data.replace(row, "")
+                validation = True
+        if not validation:
+            return "", "Couldnt find the style in the csv, try pressing the refresh button"
+        data.replace("\n\n", "\n")
+        with open(path.joinpath("styles.csv"), "w", encoding="utf-8") as f:
+            f.write(data)
+        return "", "Style deleted successfully!"
+    else:
+        return "", "Invalid action"
 def VerifyCSV(val):
     goodRow = []
     val.replace(",\n", "\n")
